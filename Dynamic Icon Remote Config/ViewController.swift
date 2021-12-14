87f147768e20bcfbe8d769dd70c1237025701b34
Ömer Hamid Kamışlı <hamid.kamisli@magis.com.tr>
2021-12-15 01:50:43 +0300

    Initial commit, pod install firebase remote config, icons added

---
+++ Dynamic Icon Remote Config/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynamic Icon Remote Config
+//
+//  Created by Ömer Hamid Kamışlı on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
